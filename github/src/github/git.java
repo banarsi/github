2c4319c4334582c78fc43e9dae5c415fdc42ff11
tyss <tyss@104.239.213.7>
2018-05-11 13:42:25 +0530

    jjjjjj

--- github/src/github/git.java
+++ github/src/github/git.java
@@ -11,11 +11,6 @@ public class git
 		System.out.println("hi");
 		System.out.println("hello eclipse");
 
-		System.out.println("bye eclipse");
-
-		
-		System.out.println("hello git");
-		System.out.println("hello github");
 		
 		
 		
