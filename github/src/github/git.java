a96f825f27f3f93f23894221a8ce26309eb12f53
banarsi <36882931+banarsi@users.noreply.github.com>
2018-05-11 12:59:52 +0530

    gitchanges

--- github/src/github/git.java
+++ github/src/github/git.java
@@ -9,13 +9,9 @@ public class git
 		System.out.println("hello");
 		
 		System.out.println("hi");
-		System.out.println("hello eclipse");
-
-		System.out.println("bye eclipse");
+		System.out.println("hello github");
 
 		
-		System.out.println("hello git");
-		System.out.println("hello github");
 		
 		
 		
